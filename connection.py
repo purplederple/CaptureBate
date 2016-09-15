b4a2f12d355bf1442a27224f3c8d42c4b14e6280
purplederple <purplederple@takeontom.com>
2016-09-15 22:38:53 +0100

    Fixed crash on bad connection

--- connection.py
+++ connection.py
@@ -6,43 +6,40 @@ from time import sleep
 import requests
 from MyAdapter import MyAdapter
 
+
 def Connection():
-	#Connecting to server
-	count = 0
-	while True:
-		try:
-			logging.info('Connecting to ' + URL)
-			client = requests.session()
-			client.mount('https://', MyAdapter())
-			# Retrieve the CSRF token first
-			r1 = client.get(URL)
-			break
-		except Exception, e:
-			logging.error('Some error during connecting to '+URL)
-			logging.error(e)
-			logging.error('Trying again after 60 seconds')
-			count+=1
-			if count > 5:
-				logging.error('Performing delay for 1800 seconds')
-				sleep(1800)
-				count = 0
-			sleep(60)
+    # Connecting to server
+    count = 0
+    while True:
+        try:
+            logging.info('Connecting to ' + URL)
+            client = requests.session()
+            client.mount('https://', MyAdapter())
+            # Retrieve the CSRF token first
+            r1 = client.get(URL)
+            break
+        except Exception, e:
+            logging.error('Some error during connecting to ' + URL)
+            logging.error(e)
+            logging.error('Trying again after 60 seconds')
+            count += 1
+            if count > 5:
+                logging.error('Performing delay for 1800 seconds')
+                sleep(1800)
+                count = 0
+            sleep(60)
 
-	csrftoken = r1.cookies['csrftoken']
-	# Set login data and perform submit
-	login_data = dict(username=USER, password=PASS, csrfmiddlewaretoken=csrftoken, next='/')
-	try:
-		r = client.post(URL, data=login_data, headers=dict(Referer=URL))
-	except Exception, e:
-		logging.error('Some error during posting to '+URL)
-		logging.error(e)		
-	#logging.debug('Page Source for ' + URL + '\n' + r.text)
-	page_source = 'Page Source for ' + URL + '\n' + r.text
-	# if Debugging is enabled Page source goes to debug.log file
-	if Debugging == True:
-		Store_Debug(page_source, "connection.log")
-	return client		
-
-
-
-
+    csrftoken = r1.cookies['csrftoken']
+    # Set login data and perform submit
+    login_data = dict(username=USER, password=PASS, csrfmiddlewaretoken=csrftoken, next='/')
+    try:
+        r = client.post(URL, data=login_data, headers=dict(Referer=URL))
+        # logging.debug('Page Source for ' + URL + '\n' + r.text)
+        page_source = 'Page Source for ' + URL + '\n' + r.text
+        # if Debugging is enabled Page source goes to debug.log file
+        if Debugging is True:
+            Store_Debug(page_source, "connection.log")
+    except Exception, e:
+        logging.error('Some error during posting to ' + URL)
+        logging.error(e)
+    return client
